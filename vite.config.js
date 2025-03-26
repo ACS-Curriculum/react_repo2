2571a86646169cac80ac259bd0778b474fd04a04
ThomasReinartz <thomasreinartz@pop-os.localdomain>
2025-03-26 07:27:15 -0500

    fifth

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig ({
-<<<<<<< HEAD
-  base: '/react_repo2/',
-  plugins: [react ()],
-=======
   plugins: [react ()],
   base: '/react_repo2/',
->>>>>>> 3e55b07 (third)
 });
